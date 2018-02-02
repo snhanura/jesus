e26024a5888b2bdfe32514feb8fe1cb43ae58561
snhanura <31534294+snhanura@users.noreply.github.com>
2018-02-02 14:21:06 +0530

    Create g59.c

---
+++ g59.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,n;
+for(i=0;i<n;i++)
+{
+scanf("%d",&a);
+}
+h=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]>h)
+{
+h=a[i];
+}
+}
+printf("%d",a[i]);
+return 0;
+}
