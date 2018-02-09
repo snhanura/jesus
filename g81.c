11c1ff78700ca9e594f7540e2aa906e11a5c7374
snhanura <31534294+snhanura@users.noreply.github.com>
2018-02-09 14:23:27 +0530

    Create g81.c

---
+++ g81.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int kw,ow,n,d;
+printf("enter the lines");
+scanf("%d",n);
+printf("the number og worriers in each line");
+for(i=o;i<n;i++)
+{
+scanf("%d%d",&kw,&ow);
+}
+printf("the difference between worriers");
+for(i=0;i<n;i++)
+{
+d=kw-ow;
+printf("/n",d);
+}
+}
