3bbaa88bb339ff66111abbcd58b3025b765a29e4
snhanura <31534294+snhanura@users.noreply.github.com>
2018-02-12 16:16:49 +0530

    Create g85.c

---
+++ g85.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s[10],s1[10.s2[10];
+int i=0,j=0,k=0;
+printf("enter the string");
+scanf("%s",&s);
+while(s[i]!='/0')
+{
+if(i%2==0)
+{
+s1[j]=s[i];
+printf("%c",s[j]);
+j++;
+}
+else
+{
+s2[k]=s[i];
+printf("%c",s[j]);
+k++;
+}}
+}
