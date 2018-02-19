4cd145db9b5edeae538e0598e33c77d43d02e59e
snhanura <31534294+snhanura@users.noreply.github.com>
2018-02-19 16:08:10 +0530

    Update g79.c

--- g79.c
+++ g79.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
-int main()
+void main()
 {
-int n;
-printf("enter the value");
-scanf("%d",&n);
+int a,b,c;
+printf("Enter 2 numbers");
+scanf("%d\t%d",&a,&b);
+if((a!=0)&&(b!=0))
+{
+c=a*b;
+for(i=0;i<=a;i++)
+{
+  if(c==(i*i))
+  {
+    printf("It is a Square");
+  }
+  else
+  {
+    printf("It is not a square");
+  }
+}
