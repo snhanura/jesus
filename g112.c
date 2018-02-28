115ed30e3406dec1811a9fa49e5909974af7b36e
snhanura <31534294+snhanura@users.noreply.github.com>
2018-02-28 08:19:17 -0800

    Create g112.c

---
+++ g112.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[10],k;
+printf("enter the number and search element");
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+if(a[i]==k)
+{
+printf("yes");
+}
+}
+}
