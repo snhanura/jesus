315be3a781e703c19ab032206000c905fdecf539
snhanura <31534294+snhanura@users.noreply.github.com>
2018-02-28 15:53:19 +0530

    Create g98.c

---
+++ g98.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+void mian()
+{
+int k,i,j,n;
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+scanf("%d",n);
+}
+l=a[0];
+for(i=0;i<k;i++)
+{
+for(j=0;j<k;j++)
+{
+if(a[i]>a[j])
+{
+printf("%d",a[i]);
+}}}
+}
+
